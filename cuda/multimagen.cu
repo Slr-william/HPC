2f92ed084cd242f527543cfb0cd59828ec528c1a
slr-william <w.as.g@utp.edu.co>
2017-03-15 10:34:05 -0500

    compiling

--- cuda/multimagen.cu
+++ cuda/multimagen.cu
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <string>
-#include <opencv2\opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <cuda.h>
 
 using namespace std;
