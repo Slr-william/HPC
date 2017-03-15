9dc080f60a9dadb50ee567412a1cbc635b90e087
slr-william <w.as.g@utp.edu.co>
2017-03-15 12:34:34 -0500

    update

--- cuda/multimagen.cu
+++ cuda/multimagen.cu
@@ -8,7 +8,7 @@
 using namespace cv;
 using namespace std;
 
-__global__ void PictureKernell(float * d_Pin, float * d_Pout, int n, int m ){
+__global__ void PictureKernell(unsigned char * d_Pin, unsigned char * d_Pout, int n, int m ){
 	int Row = blockIdx.y*blockDim.y + threadIdx.y;
 
 	int Col = blockIdx.x*blockDim.x + threadIdx.x;
