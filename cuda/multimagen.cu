ed291f73cd2924a7c0695522c63a4e7a40f825a2
William Salazar <w.as.g@utp.edu.co>
2017-03-15 14:41:32 -0500

    Update multimagen.cu

--- cuda/multimagen.cu
+++ cuda/multimagen.cu
@@ -44,6 +44,8 @@ int main(int argc, char *argv[]){
 	if (argc > 1){
 		name = string(argv[1]);
 	}
+	
+	cout<<"Name: "<<name<<endl;
 
 	Mat image = imread(name, CV_LOAD_IMAGE_GRAYSCALE);
 	int height = image.rows;
