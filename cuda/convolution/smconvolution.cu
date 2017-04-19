42c7bf232cb25969dd561885af462bad951ca6d7
slr-william <w.as.g@utp.edu.co>
2017-04-19 09:31:23 -0500

    shared memory convolution

--- cuda/convolution/smconvolution.cu
+++ cuda/convolution/smconvolution.cu
@@ -8,9 +8,13 @@
 #define RED 2
 #define GREEN 1
 #define BLUE 0
+#define TILE_SIZE 32
+
+#define MASK_WIDTH 3
+
+__constant__ char M[MASK_WIDTH*MASK_WIDTH];
 
 using namespace cv;
-
 
 __device__ unsigned char clamp(int value){
     if(value < 0)
@@ -22,37 +26,58 @@ __device__ unsigned char clamp(int value){
 }
 
 
-__global__ void sobelFilter(unsigned char *imageInput, int width, int height, unsigned int maskWidth, char *M, unsigned char *imageOutput){
-    unsigned int row = blockIdx.y*blockDim.y+threadIdx.y;
-    unsigned int col = blockIdx.x*blockDim.x+threadIdx.x;
+__global__ void sobelFilterSM(unsigned char *imageInput, int width, int height, \
+        unsigned int maskWidth,unsigned char *imageOutput){
+    __shared__ float N_ds[TILE_SIZE + MASK_WIDTH - 1][TILE_SIZE+ MASK_WIDTH - 1];
+    int n = maskWidth/2;
+    int dest = threadIdx.y*TILE_SIZE+threadIdx.x, destY = dest / (TILE_SIZE+MASK_WIDTH-1), destX = dest % (TILE_SIZE+MASK_WIDTH-1),
+        srcY = blockIdx.y * TILE_SIZE + destY - n, srcX = blockIdx.x * TILE_SIZE + destX - n,
+        src = (srcY * width + srcX);
+    if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+        N_ds[destY][destX] = imageInput[src];
+    else
+        N_ds[destY][destX] = 0;
 
-    int Pvalue = 0;
-
-    int N_start_point_row = row - (maskWidth/2);
-    int N_start_point_col = col - (maskWidth/2);
-
-    for(int i = 0; i < maskWidth; i++){
-        for(int j = 0; j < maskWidth; j++ ){
-            if((N_start_point_col + j >=0 && N_start_point_col + j < width) && (N_start_point_row + i >=0 && N_start_point_row + i < height)){
-                Pvalue += imageInput[(N_start_point_row + i)*width+(N_start_point_col + j)] * M[i*maskWidth+j];
-            }
-        }
+    // Second batch loading
+    dest = threadIdx.y * TILE_SIZE + threadIdx.x + TILE_SIZE * TILE_SIZE;
+    destY = dest /(TILE_SIZE + MASK_WIDTH - 1), destX = dest % (TILE_SIZE + MASK_WIDTH - 1);
+    srcY = blockIdx.y * TILE_SIZE + destY - n;
+    srcX = blockIdx.x * TILE_SIZE + destX - n;
+    src = (srcY * width + srcX);
+    if (destY < TILE_SIZE + MASK_WIDTH - 1) {
+        if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+            N_ds[destY][destX] = imageInput[src];
+        else
+            N_ds[destY][destX] = 0;
     }
-    imageOutput[row*width+col] = clamp(Pvalue);
+    __syncthreads();
+
+    int accum = 0;
+    int y, x;
+    for (y = 0; y < maskWidth; y++)
+        for (x = 0; x < maskWidth; x++)
+            accum += N_ds[threadIdx.y + y][threadIdx.x + x] * M[y * maskWidth + x];
+    y = blockIdx.y * TILE_SIZE + threadIdx.y;
+    x = blockIdx.x * TILE_SIZE + threadIdx.x;
+    if (y < height && x < width)
+        imageOutput[(y * width + x)] = clamp(accum);
+    __syncthreads();
 }
+
 
 __global__ void img2gray(unsigned char *imageInput, int width, int height, unsigned char *imageOutput){
     int row = blockIdx.y*blockDim.y+threadIdx.y;
     int col = blockIdx.x*blockDim.x+threadIdx.x;
 
     if((row < height) && (col < width)){
-        imageOutput[row*width+col] = imageInput[(row*width+col)*3+RED]*0.299 + imageInput[(row*width+col)*3+GREEN]*0.587 
+        imageOutput[row*width+col] = imageInput[(row*width+col)*3+RED]*0.299 + imageInput[(row*width+col)*3+GREEN]*0.587
         + imageInput[(row*width+col)*3+BLUE]*0.114;
     }
 }
 
 
 int main(int argc, char **argv){
+    cudaSetDevice(0);//GTX980
     cudaError_t error = cudaSuccess;
     clock_t start, end;
     double cpu_time_used;
@@ -100,14 +125,15 @@ int main(int argc, char **argv){
     error = cudaMemcpy(d_Mask, h_Mask, sizeof(char)*9, cudaMemcpyHostToDevice);
     if(error != cudaSuccess){printf("Error sending data from host to device in Mask \n");exit(-1);}
 
-    int blockSize = 32;
+
+    int blockSize = TILE_SIZE;
     dim3 dimBlock(blockSize, blockSize, 1);
     dim3 dimGrid(ceil(width/float(blockSize)), ceil(height/float(blockSize)), 1);
     img2gray<<<dimGrid, dimBlock>>>(d_dataImage, width, height, d_imageOutput);
     cudaDeviceSynchronize();
 
     cudaEventRecord(startGPU);
-    sobelFilter<<<dimGrid, dimBlock>>>(d_imageOutput, width, height, 3, d_Mask, d_sobelOutput);
+    sobelFilterSM<<<dimGrid, dimBlock>>>(d_imageOutput, width, height, MASK_WIDTH, d_sobelOutput);
     cudaDeviceSynchronize();
     cudaEventRecord(stopGPU);
 
@@ -129,26 +155,24 @@ int main(int argc, char **argv){
     end = clock();
 
 
-    imwrite("./sobel.jpg", gray_image);
+    imwrite("./Sobel_Image.jpg",gray_image);
 
-    namedWindow(imageName, WINDOW_NORMAL);
-    namedWindow("Gray Image CUDA", WINDOW_NORMAL);
-    namedWindow("Sobel Image OpenCV", WINDOW_NORMAL);
+   namedWindow(imageName, WINDOW_NORMAL);
+   namedWindow("Gray Image CUDA", WINDOW_NORMAL);
+   namedWindow("Sobel Image OpenCV", WINDOW_NORMAL);
+   imshow(imageName,image);
+   imshow("Gray Image CUDA", gray_image);
+   imshow("Sobel Image OpenCV",abs_grad_x);
+   waitKey(0);
 
-    imshow(imageName, image);
-    imshow("Gray Image CUDA", gray_image);
-    imshow("Sobel Image OpenCV", abs_grad_x);
-
-    waitKey(0);
-
-    free(h_dataImage);
+    free(dataRawImage);
     free(h_imageOutput);
     cpu_time_used = ((double) (end - start)) /CLOCKS_PER_SEC;
-    printf("Time in CPU: %.10f, time in GPU: %.10f\n", cpu_time_used, milliseconds);
+    printf("CPU: %.10f, GPU: %.10f\n",cpu_time_used, milliseconds);
 
-    cudaFree(d_dataImage);
+    cudaFree(d_dataRawImage);
     cudaFree(d_imageOutput);
-    cudaFree(d_Mask);
+    cudaFree(M);
     cudaFree(d_sobelOutput);
     return 0;
 }
