aa68f91b1b3f4c6da8280f1e6aa89b3dc9db1718
slr-william <w.as.g@utp.edu.co>
2017-05-18 17:30:22 -0500

    test

--- tone-mapping/cudaTone.cu
+++ tone-mapping/cudaTone.cu
@@ -33,8 +33,6 @@ int main(int argc, char *argv[]){
     if (argc > 1){
         name = string(argv[1]);
     }
-    
-    cout<<"Name: "<<name<<endl;
 
     Mat image = imread(name, 1);
     int height = image.rows;
